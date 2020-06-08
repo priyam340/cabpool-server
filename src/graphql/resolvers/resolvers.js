132408fd534d4ab0bc332ff627a9e7a5ac12827e
Priyam <priyamgupta340@gmail.com>
2020-06-08 18:55:17 +0530

    Conflict resolved

--- src/graphql/resolvers/resolvers.js
+++ src/graphql/resolvers/resolvers.js
@@ -2,10 +2,11 @@ const { sendEmailVerify } = require("./sendEmailVerify.resolver");
 const { userExists } = require("./userExists.resolver");
 const { linkGenerate } = require("./linkGenerate.resolver");
 const { sendMessage } = require("./sendMessage.resolver");
+const { signUp } = require("./signUp.resolver");
 const { getChat } = require("./getChat.resolver");
 module.exports = {
   Query: { userExists, getChat },
-  Mutation: { sendEmailVerify, linkGenerate, sendMessage },
+  Mutation: { sendEmailVerify, linkGenerate, sendMessage, signUp },
   Subscription: {
     getChatDetails: {
       subscribe: (_, { link }, { pubsub }) => {
