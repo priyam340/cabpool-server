c4bc91aadcae6e2f37bf3daed54f54689c7e65a5
Priyam <studentvit0@gmail.com>
2021-01-16 16:05:46 +0530

    First check

--- src/graphql/resolvers/userExists.resolver.js
+++ src/graphql/resolvers/userExists.resolver.js
@@ -1,6 +1,33 @@
 const User = require("../../models/user.model");
+const mysql = require("mysql");
 exports.userExists = async (_, { email }) => {
-  const user = await User.find({ email: email }).exec();
+  const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "java",
+    port: "3306",
+  });
+  conn.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to MySQL Server!");
+  });
+  var res = 0;
+  function saveIt(results) {
+    res = results;
+    console.log(results);
+  }
+  var res = 0;
+  let sql = "select * from corona ";
+  async function ch() {
+    try {
+      const user = await conn.query(sql);
+    } catch (err) {}
+  }
+
   console.log(user);
-  return user.length == 0 ? false : true;
+  return true;
+  // const user = await User.find({ email: email }).exec();
+  // console.log(user);
+  // return user.length == 0 ? false : true;
 };
